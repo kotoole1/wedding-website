dac4741678bc26b5a87b43b9a1a389350446b9c6
Kevin O'Toole <k.otoole01@gmail.com>
2023-03-06 10:07:37 -0500

    starting dividers

--- js/scripts.js
+++ js/scripts.js
@@ -153,7 +153,7 @@ $(document).ready(function () {
                 if (target.length) {
                     $('html,body').animate({
                         scrollTop: target.offset().top - 90
-                    }, 2000);
+                    }, 300);
                     return false;
                 }
             }
